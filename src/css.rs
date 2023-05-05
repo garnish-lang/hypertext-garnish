eb230b6226441a37cf98f0dcb08ee1e6d4851bcd
Chad Collins <chadrcollins91@gmail.com>
2023-05-05 11:46:03 -0400

    Added Selector enum

--- src/css.rs
+++ src/css.rs
@@ -3,8 +3,29 @@ struct Declaration {
     value: String,
 }
 
+enum CombinatorSelector {
+    Descendant(Selector),
+    Child(Selector),
+    AdjacentSibling(Selector),
+    GeneralSibling(Selector),
+}
+
+enum Selector {
+    Tag(String), // tag name
+    Class(String), // class name
+    Id(String), // id name
+    Combinator(Selector, CombinatorSelector), // (base selector, combination)
+    PseudoClass(Selector, String), // (base selector, pseudo class)
+    PseudoElement(Selector, String), // (base selector, pseudo element)
+    Attribute(String), // attribute name
+    AttributeValue(String, String), // (attribute name, attribute value)
+    AttributeContains(String, String), // (attribute name, search string)
+    Chain(Vec<Selector>), // no space merge (e.g. p.my-class[someAttribute])
+    Group(Vec<Selector>), // comma separated list (e.g. body, h1, p)
+}
+
 struct Rule {
-    selector: String,
+    selector: Selector,
     declarations: Vec<Declaration>,
     sub_rules: Vec<Rule>,
 }
